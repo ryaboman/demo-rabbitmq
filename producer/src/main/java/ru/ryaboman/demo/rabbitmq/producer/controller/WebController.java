c61c4e11bda9a4a3f289075db440ab09863cb8a1
Aleksey <mr.ryabov.aa@yandex.ru>
2025-07-18 02:33:53 +0300

    Добавляет настройки RabbitMQ

--- producer/src/main/java/ru/ryaboman/demo/rabbitmq/producer/controller/WebController.java
+++ producer/src/main/java/ru/ryaboman/demo/rabbitmq/producer/controller/WebController.java
@@ -1,13 +1,24 @@
 package ru.ryaboman.demo.rabbitmq.producer.controller;
 
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class WebController {
 
+    @Autowired
+    private RabbitTemplate rabbitTemplate;
+
     @GetMapping("/")
     public String showForm() {
         return "index";
+    }
+
+    @MessageMapping("/send")
+    public void handleMessage(String message) {
+        rabbitTemplate.convertAndSend("hello", message); // Отправка в RabbitMQ
     }
 }
